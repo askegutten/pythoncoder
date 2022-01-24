90b84318128d6d3d446fdaeebe6d327a466d158f
askegutten <45385279+askegutten@users.noreply.github.com>
2022-01-24 11:08:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 # oppgave 1
 
 print("hello world")
+
+from flask importFlask, render_template
+
+app=Flask(__name__)
+
+@app.route(‘/’)
+
+def home():
+  return render_template(“index.html”)
+
+if __name__ ==’__main__’:
+  app.run(debug=True)
